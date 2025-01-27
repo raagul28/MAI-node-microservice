e621aac40c728049df77a021338607fc75387d8e
raagul28 <raagulraagul0306@gmail.com>
2025-01-27 10:46:49 +0530

    Added welcome page and HI MAI page functionality

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 
-
+// Route for the welcome page
 app.get('/', (req, res) => {
-    res.render('index', { message: 'Hi MAI!' }); 
+    res.render('welcome'); 
 });
 
+// Route for the "HI MAI!" page
+app.get('/hi', (req, res) => {
+    res.render('hi', { message: 'Hi MAI!' });
+});
 
+// Health check endpoint
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'ok' }); 
 });
